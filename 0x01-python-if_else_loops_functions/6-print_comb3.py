3adff1e10561e7b22d454cdae41a1e01c320fc1f
Kolawole Isaac <Ikolawole@wazobia.tech>
2024-02-11 20:24:05 +0100

    add number comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,7 @@
 start = 10
 for i in range(start):
     for e in range(i + 1, start):
+        if (i == 8):
+            print("{:d}{:d}".format(i, e))
+            break
         print("{:d}{:d}".format(i, e), end=", ")
-

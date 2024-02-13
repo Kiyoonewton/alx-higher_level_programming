ab0cddcafd3806c1c92f8fc7bc47db71f462bb1a
Kolawole Isaac <Ikolawole@wazobia.tech>
2024-02-13 02:48:54 +0100

    remove a char index

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    str_remove = str[0:n] + str[n + 1:]
+    if (n < 0):
+        str_remove = str
+    else:
+        str_remove = str[0:n] + str[n + 1:]
     return str_remove
